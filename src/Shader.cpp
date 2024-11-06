5ea299225a618a7c6f1730acbd0b52119dfb61bd
Barti1304 <thegamer13041304@gmail.com>
2024-11-06 18:08:08 +0100

    added Shader class

---
+++ src/Shader.cpp
@@ -0,0 +1,46 @@
+#include "Shader.h"
+
+Shader::Shader(const char* vCode, const char* fCode)
+{
+
+}
+
+Shader::~Shader()
+{
+
+}
+
+void Shader::use()
+{
+
+}
+
+void Shader::setInt(const char* uName, int value)
+{
+
+}
+
+void Shader::setFloat(const char* uName, float value)
+{
+
+}
+
+void Shader::setVec2(const char* uName, glm::vec2 value)
+{
+
+}
+
+void Shader::setVec3(const char* uName, glm::vec3 value)
+{
+
+}
+
+void Shader::setVec4(const char* uName, glm::vec4 value)
+{
+
+}
+
+void Shader::setMat4(const char* uName, glm::mat4 value)
+{
+
+}
---
+++ src/Shader.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+class Shader
+{
+public:
+	Shader(const char* vCode, const char* fCode);
+	~Shader();
+
+	void use();
+
+	void setInt(const char* uName, int value);
+	void setFloat(const char* uName, float value);
+	void setVec2(const char* uName, glm::vec2 value);
+	void setVec3(const char* uName, glm::vec3 value);
+	void setVec4(const char* uName, glm::vec4 value);
+	void setMat4(const char* uName, glm::mat4 value);
+
+private:
+	unsigned int shaderID = 0;
+};
