4b91b48a50c88e848d2a8a1cb6ff9672c12b8206
Barti1304 <thegamer13041304@gmail.com>
2024-11-11 13:31:52 +0100

    added getters and setters to GameObject class

---
+++ src/GameObject.cpp
@@ -0,0 +1,35 @@
+#include "GameObject.h"
+
+void GameObject::setBodyID(b2BodyId ID)
+{
+	bodyID = ID;
+}
+
+b2BodyId GameObject::getBodyID()
+{
+	return bodyID;
+}
+
+glm::vec2 GameObject::getPosition()
+{
+	b2Vec2 pos = b2Body_GetPosition(bodyID);
+	return glm::vec2{ pos.x, pos.y };
+}
+
+glm::vec2 GameObject::getSize()
+{
+	b2ShapeId shapeID;
+	b2Body_GetShapes(bodyID, &shapeID, 1);
+	b2Polygon poly = b2Shape_GetPolygon(shapeID);
+
+	float width = glm::abs(b2Length(b2Sub(poly.vertices[0], poly.vertices[1])));
+	float height = glm::abs(b2Length(b2Sub(poly.vertices[0], poly.vertices[2])));
+
+	glm::vec2 size{};
+
+}
+
+float GameObject::getRotation()
+{
+	return b2Rot_GetAngle(b2Body_GetRotation(bodyID));
+}
--- src/GameObject.h
+++ src/GameObject.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <box2d/box2d.h>
+#include <glm/glm.hpp>
 
 class GameObject
 {
@@ -8,6 +9,16 @@ public:
 	GameObject() = default;
 	virtual ~GameObject() = 0;
 
-private:
+	virtual void update() = 0;
+
+	void setBodyID(b2BodyId ID);
+
+	b2BodyId getBodyID();
+	glm::vec2 getPosition();
+	glm::vec2 getSize();
+	float getRotation();
+
+protected:
 	const char* objectID;
+	b2BodyId bodyID;
 };
